fe5df8d238b58175eddf7d50affaa6aefdc0880b
yk-amarly-20 <y.k.83600260@icloud.com>
2021-01-29 01:57:59 +0900

    feat: change rich menu image

--- package/rich_menu.py
+++ package/rich_menu.py
@@ -29,7 +29,7 @@ def createRichMenu(line_bot_api: LineBotApi) -> bool:
 
         richMenuId = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
 
-        path = "images/test.jpg"
+        path = "images/rich_menu_v1.png"
 
         if os.path.exists(path):
             with open(path, "rb") as f:
