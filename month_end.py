56135735cb09fcb9d5da5268eb5a5b77b3d3dc6e
yk-amarly-20 <y.k.83600260@icloud.com>
2021-02-05 15:19:04 +0900

    fix: change location of 'create_table_name'

--- month_end.py
+++ month_end.py
@@ -1,4 +1,4 @@
-from db import *
+from package.db import *
 import datetime
 
 # 定期実行するスクリプト
@@ -9,11 +9,6 @@ def get_year_and_month():
     month = current_time.month
 
     return year, month
-
-def create_table_name(year: int, month: int) -> str:
-    table_name = "log_" + str(year) + "_" + str(month)
-
-    return table_name
 
 def main():
     year, month = get_year_and_month()
